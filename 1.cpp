30d9798785763568965b29fc5b6e382826794c3f
Adidala Divishath Reddy <48645763+Adidala-Divishath-Reddy@users.noreply.github.com>
2022-12-10 23:07:31 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -11,9 +11,13 @@ void solve(vector<vector<int>> &matrix){
                 dfs(image, i, j, minRow, maxRow, minCol, maxCol);
                 float centreX, centreY;
                 int width, height;
+                // centre (x,y) will be----> x = (midpoint of left most value and right most value, midpoint of top most value and bottom most value)  
                 centreY = (float)minRow + (float)((float)maxRow - (float)minRow)/2.0;
                 centreX = (float)minCol + (float)((float)maxCol - (float)minCol)/2.0;
+                
+                //width will be absolute difference of left most value and right most value
                 width = maxCol - minCol + 1;
+                //height will be absolute difference of top most value and bottom most value
                 height = maxRow - minRow + 1;
                 cout<<"["<<centreX << ","<<centreY<<","<< width<<","<<height<<"]";
             }
@@ -26,9 +30,13 @@ void dfs(vector<vector<int>> &image, int i , int j, int &minRow, int &maxRow, in
         return;
     
     image[i][j] = 0;
+    // Taking the top most enabled pixel
     minRow = min(minRow, i);
+    // Taking the bottom most enabled pixel
     maxRow = max(maxRow, i);
+    // Taking the left most enabled pixel
     minCol = min(minCol, j);
+    // Taking the right most enabled pixel
     maxCol = max(maxCol, j);
     
     dfs(image, i - 1, j, minRow, maxRow, minCol, maxCol);
